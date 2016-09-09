59033b479d5c580aea71e3a743c82d0edefca1e9
Jayesh Kawli <j.kawli@gmail.com>
2016-09-08 23:24:12 -0400

    Added support to show legend view on the spider chart side menu

--- JKScratchPad/ViewController.m
+++ JKScratchPad/ViewController.m
@@ -15,17 +15,22 @@
 
 @implementation ViewController
 
-#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:0.7]
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     CGFloat maxRadius = 160;
-    UIView* v = [[UIView alloc] initWithFrame:CGRectMake(0, 0, maxRadius * 2, maxRadius * 2)];
+    UIView* v = [[UIView alloc] initWithFrame:CGRectMake(0, 0, (maxRadius * 2) + 44, maxRadius * 2)];
     v.layer.borderColor = [UIColor darkGrayColor].CGColor;
     v.layer.borderWidth = 2.0;
     v.center = CGPointMake(200, 400);
     [self.view addSubview:v];
+    
+    UIView* legendView = [[UIView alloc] initWithFrame:CGRectMake(maxRadius * 2, 0, 44, maxRadius * 2)];
+    legendView.backgroundColor = [UIColor blueColor];
+    [v addSubview:legendView];
+    
     CGFloat center = maxRadius;
     
     CAShapeLayer* layer = [self createPieSliceWithRadius:100 startAngle:0 andEndAngle:60 andCenter:CGPointMake(center, center) andColor:UIColorFromRGB(0x2ecc71)];
@@ -44,23 +49,27 @@
     [v.layer addSublayer:layer3];
     [v.layer addSublayer:layer4];
     
+    [layer addSublayer:[self labelForLayerWithCenter:CGPointMake(center, center) andRadius:100 andStartAngle:0 andEndAngle:60]];
+    [layer1 addSublayer:[self labelForLayerWithCenter:CGPointMake(center, center) andRadius:60 andStartAngle:60 andEndAngle:210]];
     [layer2 addSublayer:[self labelForLayerWithCenter:CGPointMake(center, center) andRadius:140 andStartAngle:210 andEndAngle:310]];
+    [layer3 addSublayer:[self labelForLayerWithCenter:CGPointMake(center, center) andRadius:160 andStartAngle:310 andEndAngle:330]];
 }
 
 - (CATextLayer*)labelForLayerWithCenter:(CGPoint)center andRadius:(CGFloat)radius andStartAngle:(CGFloat)startAngle andEndAngle:(CGFloat)endAngle {
     CATextLayer *label = [[CATextLayer alloc] init];
     label.fontSize = 12;
-    label.backgroundColor = [UIColor blackColor].CGColor;
-    NSLog(@"%f", cos(DEG2RAD(135)));
-    CGFloat val = cos(DEG2RAD(135));
-    CGFloat angles = startAngle + (endAngle - startAngle) /2.0;
-    CGFloat anglec = cos(DEG2RAD(angles));
-    CGFloat angless = sin(DEG2RAD(angles));
-    label.frame = CGRectMake(center.x + ((radius/2.0) * anglec) - 10, center.y + ((radius/2.0) * angless) - 10, 20, 20);
+    CGFloat angle = startAngle + (endAngle - startAngle) /2.0;
+    CGFloat angleCos = cos(DEG2RAD(angle));
+    CGFloat angleSin = sin(DEG2RAD(angle));
+    CGFloat xLabelOffset = 15;
+    CGFloat yLabelOffset = 8;
+    
+    label.frame = CGRectMake(center.x - xLabelOffset + ((radius + 10)) * angleCos, center.y - yLabelOffset + ((radius + 10)) * angleSin, 30, 16);
     label.contentsScale = [UIScreen mainScreen].scale;
-    [label setString:@"Hello"];
+    NSString* percentage = [NSString stringWithFormat:@"%ld%%", (long)(((endAngle - startAngle) / 360.0) * 100)];
+    [label setString:percentage];
     [label setAlignmentMode:kCAAlignmentCenter];
-    [label setForegroundColor:[[UIColor whiteColor] CGColor]];
+    [label setForegroundColor:[[UIColor blackColor] CGColor]];
     label.zPosition = 100;
     return label;
 }
@@ -76,6 +85,10 @@
     anim.fillMode = kCAFillModeForwards;
     CAShapeLayer *slice = [CAShapeLayer layer];
     slice.fillColor = color.CGColor;
+    slice.shadowOffset = CGSizeMake(5, 5);
+    slice.shadowRadius = 10.0;
+    slice.shadowColor = [UIColor lightGrayColor].CGColor;
+    slice.masksToBounds = NO;
     slice.strokeColor = [UIColor lightGrayColor].CGColor;
     slice.lineWidth = 1.0;
     slice.path = toPath;
